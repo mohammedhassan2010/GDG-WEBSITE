22d763d2fcbbfc1391e1db76e3f15c31523e314d
Mohammedhassan <mohammedhassann@outlook.com>
2023-05-17 19:56:13 +0300

    gdg website

---
+++ css/js/script.js
@@ -0,0 +1,9 @@
+const text =Document.qureselector(".text")
+const red =Document.qureselector(".red")
+const blue =Document.qureselector(".blue")
+const green =Document.qureselector(".green")
+function change (){
+    red.onclick(
+        style.color="red"
+    )
+}
